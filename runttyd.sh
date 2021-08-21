a91d283c6d3497a78f17a38cb231b71f4804848a
neveisa <sbmohant@usc.edu>
2021-08-21 04:31:04 +0000

    added run scripts

---
+++ runttyd.sh
@@ -0,0 +1,1 @@
+./ttyd.x86_64 --ssl --ssl-cert tutorialwebsite/setup/server/wettyssl/fullchain.pem --ssl-key tutorialwebsite/setup/server/wettyssl/privkey.pem --port 2501 bash -c "ssh -C trojan@192.168.250.250"
